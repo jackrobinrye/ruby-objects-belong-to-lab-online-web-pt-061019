98c27bdb67dad6ef26f01e63a8be41913bec0e05
Jack Atherton <jackrrye@gmail.com>
2019-06-21 00:34:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,9 @@ require "pry"
 class Artist
   attr_accessor :name, :genre 
   
-  def initialize(name, genre)
-    @name = name
-    @genre = genre
-  end 
+  # def initialize(name, genre)
+  #   @name = name
+  #   @genre = genre
+  # end 
   
 end 
